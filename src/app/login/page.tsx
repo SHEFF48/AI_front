c1d3605c80a932ab3f34a528be7b23add07004e1
SHEFF48 <sun-service@ukr.net>
2024-04-21 21:03:04 +0300

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,12 +2,15 @@ import LoginForm from "@/components/custom/forms/Login";
 import RegistartionForm from "@/components/custom/forms/Registartion";
 import RecoveryForm from "@/components/custom/forms/Recovery";
 import CreateForm from "@/components/custom/forms/Create";
+import { Suspense } from "react";
 
 export default function Create() {
   return (
     <main className="flex flex-col min-h-screen  items-center justify-center overflow-hidden">
       <div className="w-[416px] p-7 rounded-2xl shadow">
-        <LoginForm />
+        <Suspense fallback={<div>Loading...</div>}>
+          <LoginForm />
+        </Suspense>
       </div>
     </main>
   );
