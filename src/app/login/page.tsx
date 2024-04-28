479cf8efca9600f53fdeb797a5db417442f797a9
SHEFF48 <sun-service@ukr.net>
2024-04-28 11:59:52 +0300

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -10,7 +10,7 @@ import { authConfig } from "../configs/auth";
 export default async function page() {
   const session = await getServerSession(authConfig);
 
-  session?.user && redirect("/messages");
+  session?.user && redirect("/t");
 
   return (
     <main className="flex flex-col min-h-screen  items-center justify-center overflow-hidden">
